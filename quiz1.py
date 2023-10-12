69a4b19c79f57aa93d2f9b12c5aae9d041780e59
dave nooxonn lora <davelora143@gmail.com>
2023-10-12 05:29:09 -0700

    first_commit

---
+++ quiz1.py
@@ -0,0 +1,8 @@
+diesel_price = 70
+no_liter = int (input ("Number of liters to buy: "))
+total_amount_to_pay = diesel_price * no_liter
+
+print("OUTPUT")
+print("Price of diesel:", diesel_price, "per liter")
+print("Number of liters:", no_liter)
+print("Total Amount to Pay:", total_amount_to_pay, "only")
